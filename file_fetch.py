85fbb17da62361f233f56ab7538c18cfe12d04fd
Tarun Kumar <tarunvelagala80@gmail.com>
2019-05-26 12:13:42 +0530

    Initial Commit

--- file_fetch.py
+++ file_fetch.py
@@ -1,34 +1,61 @@
 import inspect
 import os
 import re
-
+from collections import Counter
+from matplotlib import pyplot as plt
 import numpy
-
-os.chdir('E:\\Intern Work TeamTact')  # change the current root  directory
-np_methods_list = []
-lines_with_np_methods = []
+import pandas as pd
 
 
-def fetch_np_methods():
+def main():
+    os.chdir('E:\\Intern Work TeamTact')  # change the current root directory
+    np_methods_list = []
+    lines_with_np_methods = []
+    method_count_list = []
+    fetch_np_methods(np_methods_list)
+    file_fetch(lines_with_np_methods)
+    find_methods_in_lines(lines_with_np_methods, np_methods_list, method_count_list)
+    plot_top_20(method_count_list)
+
+
+# For extracting all the methods in Numpy Package
+def fetch_np_methods(np_methods_list):
     np_methods = inspect.getmembers(numpy, inspect.isfunction)
     for i in np_methods:
         np_methods_list.append(i[0])
-    # print(np_methods_list)
 
 
-fetch_np_methods()
-
-
-def file_fetch():
-    for path, subdirs, files in os.walk(os.getcwd()):  # looping over the files through walker
+# for fetching the files from the directories and searching for numpy entries
+def file_fetch(lines_with_np_methods):
+    # looping over the files through walker
+    for path, subdirs, files in os.walk(os.getcwd()):
         for name in files:
-            if name.endswith('.py'):  #
-                # file_list.append(os.path.join(path, name))
-                with open(os.path.join(path, name), 'r', encoding="utf8") as f:
+            if name.endswith('.py') or name.endswith('.ipynb'):  #
+                with open(os.path.join(path, name), 'r', encoding="utf8") as f:  # opening the files
                     for line in f:
-                        if re.findall(r'np\.[a-z]+\(*', line):  # finding all lines with numpy methods
-                            lines_with_np_methods.append(line.strip())
-    print(lines_with_np_methods)
+                        # finding all lines with numpy methods
+                        if re.findall(r'np\.[a-z]+\(*', line) or re.findall(r'numpy\.[a-z]+\(*',
+                                                                            line):  # searching for words like np.* or numpy.*
+                            lines_with_np_methods.append(line.strip())  # removing all the white spaces from the code
 
 
-file_fetch()
+# finding all the methods in comparision with numpy methods
+def find_methods_in_lines(lines_with_np_methods, np_methods_list, method_count_list):
+    for i in lines_with_np_methods:
+        for xs in np_methods_list:
+            if 'np.' + xs in i:
+                method_count_list.append(str(xs))
+
+
+# plot top 20 methods used in all files
+def plot_top_20(method_count_list):
+    letter_counts = Counter(method_count_list)  # Making a dictionary of frequencies of methods used
+    df = pd.DataFrame.from_dict(letter_counts, orient='index')
+    df = df.nlargest(20, df.columns)  # Finding nlargest from the entries
+    print(df)
+    df.plot(kind='bar')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ report.md
@@ -0,0 +1,44 @@
+### Assignment :
+
+1. Find any 10 public repositories(which are Numpy based)
+2. Download their code in to your machine
+3. Find how many numpy methods are used in their code by using python (just read line by line and use Regex to find numpy usage)
+4. Show the details in a report.
+
+### Data Report
+![report](myplot.png)
+
+**method**|**count**
+:-----:|:-----:
+sum|164
+asarray|142
+max|139
+all|128
+allclose|122
+argmax|121
+linspace|100
+min|85
+ones|81
+array\_equal|77
+mean|53
+prod|48
+zeros\_like|43
+meshgrid|39
+average|36
+atleast\_2d|35
+load|33
+vstack|33
+save|31
+hstack|24
+
+### Repos: 
+https://github.com/parasdahal/deepnet
+https://github.com/rougier/numpy-100
+https://github.com/Kyubyong/numpy_exercises
+https://github.com/kdexd/digit-classifier
+https://github.com/stasi009/NumpyWDL
+https://github.com/MorvanZhou/simple-neural-networks
+https://github.com/carefree0910/MachineLearning
+https://github.com/Kyubyong/tensorflow-exercises
+https://github.com/kirit93/NumpyTutorial
+https://github.com/ahmedfgad/NumPyCNN
